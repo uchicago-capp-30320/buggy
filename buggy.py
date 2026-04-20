14c1112418971f943225ec9122d6fbc2af826a67
jpt <dev@jpt.sh>
2026-04-19 23:04:12 -0500

    fix typo

--- buggy.py
+++ buggy.py
@@ -15,6 +15,8 @@ See if you can write two distinct failing tests and a passing test.
 Tip: To save time, you might want to use `pytest.mark.parametrize`
  since all of the tests will be in the format
 """
+
+
 def gcd(a: int, b: int) -> int:
     """
     Greatest common divisor between two integers.
@@ -22,7 +24,7 @@ def gcd(a: int, b: int) -> int:
     # Euclidean algorithm
     while b:
         a, b = b, a % b
-        
+
     return a
 
 
@@ -35,12 +37,15 @@ Write a @pytest.fixture that returns a test user and use that to test the two fu
 
 Each test should pass and only test one function.
 """
+
+
 def format_user_html(user_dict: dict) -> str:
-    return f"<b>{user_dict.user.name}</b><p>{{user_dict.user.profile}}</p>"
+    return f"<b>{user_dict['user']['name']}</b><p>{user_dict['user']['name']}</p><span>{user_dict['user']['profile']}"
+
 
 def validate_user_password(user_dict: dict, password: str) -> bool:
-    return user_dict.user.password == password
-    
+    return user_dict["user"]["password"] == password
+
 
 """
 3. Here is a function where the tests would fail intermittently since it depends on the time.
@@ -49,9 +54,12 @@ A refactor would make sense usually, but let's say you need to test it as-is.
 
 Write two tests that demonstrate both possible return values and pass consistently.
 """
+
+
 def close_to_midnight():
     now = datetime.datetime.now()
     return (now.hour == 23 and now.minute >= 55) or (now.hour == 0 and now.minute <= 5)
+
 
 """
 4. This function would make an http request to an API that returns JSON in the form:
@@ -62,6 +70,8 @@ It would return ["Sherlock", "Watson"]
 
 Write a passing test that mocks the `httpx.get` response.
 """
+
+
 def extract_name(url):
     resp = httpx.get(url)
     data = resp.json()
@@ -78,9 +88,11 @@ Write three passing tests that check for these conditions & the non-error case.
 
 Tip: use `pytest.raises`
 """
+
+
 def divide_all_by(nums: list[int], divisor: int) -> list[float]:
     if not nums or not all(isinstance(x, int) for x in nums):
         raise ValueError("nums must be a list of integers")
     if divisor == 0:
         raise ZeroDivisionError()
-    return [num/divisor for num in nums]
+    return [num / divisor for num in nums]
